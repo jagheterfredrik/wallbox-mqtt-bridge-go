7b9f7fe923f9eb8de32ee8bcd6445ed887423a16
jagheterfredrik <jagheterfredrik@gmail.com>
2023-12-19 15:14:11 +0100

    add stub for mq amd64

---
+++ mq_linux_amd64.go
@@ -0,0 +1,5 @@
+package main
+
+func mq_open(path []byte) uintptr                  { return 0 }
+func mq_timedsend(fd uintptr, data []byte) uintptr { return 0 }
+func mq_close(fd uintptr)                          {}
